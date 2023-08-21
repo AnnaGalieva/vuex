aec3645370f49d2312b4d3ec03914bc48e1c0950
Anna <anutasash@mail.ru>
2023-08-21 17:09:09 +0300

    seminar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Counter: {{ count }}</h2>
+
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+  <hr>
+  <div>
+    <h2>Корзина покупок</h2>
+    <ul>
+      <li v-for="item in cartItems" :key="item.id">{{ item.name }}-{{ item.price }}</li>
+    </ul>
+    <p>Общее кол-во товаров в корзине:{{ totalCartItems }}</p>
+    <p>Общая стоимость:{{ totalCartPrice }}</p>
+    <button @click="addItemToCart({ id: 1, name: 'Товар', price: 10 })">Добавить в корзину</button>
+  </div><hr>
+  <div>
+    <h2>dataFromServer</h2>
+    <ul>
+      <li v-for="item in data" :key="item">{{ item }}</li>
+    </ul>
+    <button @click="fetchData">getData</button>
+  </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+
+  },
+  computed: {
+    ...mapState(['count','data']),
+    ...mapGetters(['cartItems', 'totalCartItems', 'totalCartPrice']),
+
+  },
+  methods: {
+    ...mapMutations(['increment', 'decrement']),
+    ...mapActions(['addItemToCart','fetchData'])
+  },
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
